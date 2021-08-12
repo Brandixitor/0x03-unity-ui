291bdcb25a1a1dfce08abcbb1fa42d55055b014b
Brandixitor <med.salah.bourouba@gmail.com>
2021-08-12 03:32:28 +0100

    Done with the whole project

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,11 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+// <summary>trapMat material.</summary>
+public Material trapMat;
+
+// <summary>goalMat material.</summary>
+public Material goalMat;
+
+// <summary>Colorblind checkbox.</summary>
+public Toggle colorblindMode;
+
+
     public void PlayMaze()
     {
+        if (colorblindMode.isOn)
+        {
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
+        else
+        {
+           trapMat.color = Color.red;
+           goalMat.color = Color.green;
+        }
+        
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
